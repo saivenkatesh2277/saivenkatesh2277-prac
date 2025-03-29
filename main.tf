040a042569a05da82dab726db141b0faa903a448
saivenkatesh2277 <saivenkatesh66@gmail.com>
2025-03-29 11:27:58 +0530

    newcommitvpcmodule

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@ module "ec2_instance" {
   instance_type = "t2.micro"
   ami_id       = "ami-04b4f1a9cf54c11d0"
 }
+
+
+
+module "network" {
+  source = "./modules/vpc"
+
+  vpc_name            = var.vpc_name
+  vpc_cidr            = var.vpc_cidr
+  availability_zones  = var.availability_zones
+  private_subnets_cidr = var.private_subnets_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  tags               = var.tags
+}
